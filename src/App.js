a511c412bff723e78314fa9a4a2cb0bb9a51ecb2
Deepthikka Kambarayar <deepthikka.k@nectechnologies.in>
2021-09-07 07:23:20 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { API } from 'aws-amplify';
 
 function App() {
+
+  async function checkUser() {
+    const user =  await Auth.currentAuthenticatedUser();
+    console.log('user : ' + user)
+  }
+
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width='200' height='200' />
+        <button onClick={() => Auth.federatedSignIn()}> Sign In</button>
+        <button onClick={checkUser}> Check User</button>
+        <button onClick={() => Auth.signOut()}> Sign Out</button>
       </header>
+      <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+   </Switch>
     </div>
+    </Router>
   );
 }
 
